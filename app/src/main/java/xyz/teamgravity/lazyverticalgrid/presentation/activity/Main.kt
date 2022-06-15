37a986366857958ee4df54e0087025cb27c20dfd
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-15 14:41:11 +0500

    implement LazyVerticalGrid

--- app/src/main/java/xyz/teamgravity/lazyverticalgrid/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/lazyverticalgrid/presentation/activity/Main.kt
@@ -3,13 +3,26 @@ package xyz.teamgravity.lazyverticalgrid.presentation.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import xyz.teamgravity.lazyverticalgrid.R
 import xyz.teamgravity.lazyverticalgrid.presentation.theme.LazyVerticalGridTheme
 
 class Main : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -18,7 +31,26 @@ class Main : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    val state = rememberLazyGridState()
+                    LazyVerticalGrid(
+                        columns = GridCells.Adaptive(100.dp),
+                        state = state,
+                    ) {
+                        items(100) { index ->
+                            Card(
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .aspectRatio(1F)
+                            ) {
+                                Box(
+                                    contentAlignment = Alignment.Center,
+                                    modifier = Modifier.fillMaxSize()
+                                ) {
+                                    Text(text = stringResource(id = R.string.your_item, index.toString()))
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
